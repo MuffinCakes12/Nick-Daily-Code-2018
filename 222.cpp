5aaae2435a31f9ee2cdd19761b193bf8174d1616
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-02-22 13:06:56 -0700

    Add files via upload

--- 222.cpp
+++ 222.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
+int Area(int x, int y);
+
+int main() {
+	int a;
+	int b;
+	cout << "Please provide a value for Length" << endl;
+	cin >> a;
+	cout << "Please provide a value for Width" << endl;
+	cin >> b;
+	cout <<"The area of the rectangle is "<< Area(a, b) << endl;
+	system("pause");
+}
 int Area(int x, int y) {
 	return x*y;
-}
-int main() {
-	int x;
-	int y;
-	cout << "Please provide a value for Length" << endl;
-	cin >> x;
-	cout << "Please provide a value for Width" << endl;
-	cin >> y;
-	cout << "The Area of the Rectangle is " << x*y << endl;
-	system("pause");
 }
