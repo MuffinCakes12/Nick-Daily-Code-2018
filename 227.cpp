4c28bba59f6509fe4bee0e4ccce6121cec330507
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-02-27 13:26:29 -0700

    Add files via upload

---
+++ 227.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+	string line;
+	ifstream myfile("Pro01.in.txt");
+	if (myfile.is_open())
+	{
+			while (getline(myfile, line))
+			}
+}
