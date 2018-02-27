7970f5445602d2a3025eba4d205cb57fb7fd888c
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-02-27 14:39:34 -0700

    Add files via upload

--- Rooms.cpp
+++ Rooms.cpp
@@ -2,22 +2,24 @@
 #include <time.h>
 using namespace std;
 int main() {
-	srand(time(NULL));
+
 
 	char input = 'a';
-	int room = 1;
+	int room = 6;
 	while (input != 'q') {
-		int num = rand() % 6 + 1;
 		
+
 		switch (room) {
+
 		case 1:
-			cout << "You're in room 1, you can go (s)" << endl;
+			cout << "You're in zone 1 a cold and barren location a cold wind whips around you. 1 truly is the loneliest number, you can go (s)" << endl;
 			cin >> input;
 			if (input == 's')
 				room = 2;
 			break;
+
 		case 2:
-			cout << "You're now in room 2, you can go (w), (e), (n)" << endl;
+			cout << "You're now in zone 2 you slowly start drifting throughout space slowly, you watch as atoms collide. Are you witnessing the beginning of the universe itself? You can go (w), (e), (n)" << endl;
 			cin >> input;
 			if (input == 'e')
 				room = 4;
@@ -26,22 +28,25 @@ int main() {
 			if (input == 'n')
 				room = 1;
 			break;
+
 		case 3:
-			cout << "You're in room 3, you can go (e) or (s)" << endl;
+			cout << "You're in zone 3 you get an awkard feeling as if the space itself is watching you. As you explore something reaches out and whispers in your ear" << endl; cout<< "you can go (e) or (s)" << endl;
 			cin >> input;
 			if (input == 'e')
 				room = 2;
 			if (input == 's')
 				room = 5;
 			break;
+
 		case 4:
-			cout << "You are in room 4. You can go (w) or (s)" << endl;
+			cout << "You are in zone 4 this zone is full of water, although it feel thicker you press on as you swim it slows you down. You panic and feel as if you are about to drown" << endl; cout<<" You can go(w) or (s)" << endl;
 			cin >> input;
 			if (input == 'w')
 				room = 2;
 			if (input == 's')
 				room = 6;
 			break;
+
 		case 5:
 			cout << "You are in room 5. You can go (s) or (n)" << endl;
 			cin >> input;
@@ -50,6 +55,7 @@ int main() {
 			if (input == 'n')
 				room = 3;
 			break; 
+
 		case 6:
 				cout << "You are in room 6. You can go (s) or (n)" << endl;
 				cin >> input;
@@ -58,6 +64,7 @@ int main() {
 				if (input == 'n')
 					room = 4;
 				break;
+
 		case 7:
 			cout << "You are in room 7. You can go (n) or (e)" << endl;
 			cin >> input;
@@ -66,6 +73,7 @@ int main() {
 			if (input == 'e')
 				room = 9;
 			break;
+
 		case 8:
 			cout << "You are in room 8. You can go (n) or (w)" << endl;
 			cin >> input;
@@ -74,6 +82,7 @@ int main() {
 			if (input == 'w')
 				room = 9;
 			break;
+
 		case 9:
 			cout << "You are in room 9. You can go (w), (e), or (s)" << endl;
 			cin >> input;
@@ -84,6 +93,7 @@ int main() {
 			if (input == 's')
 				room = 10;
 			break;
+
 		case 10:
 			cout << "You are in room 10. You can go (n)" << endl;
 			cin >> input;
