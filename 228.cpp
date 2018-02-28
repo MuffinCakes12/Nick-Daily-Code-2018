5154dc7795905439aa3b38f94d9b394e694a2415
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-02-28 13:06:39 -0700

    Add files via upload

---
+++ 228.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<time.h>
+#include<ctime>
+using namespace std;
+
+
+int main() {
+	srand(time(NULL));
+
+
+
+	while (1) {
+		cout << '*';
+		int snow = rand() % 100 + 1;
+		for (int i = 0; i<snow; i++)
+			cout << " ";
+
+	}
+}
