2942c578641431d17a2d82e73f2ac79279a53b79
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-03-06 13:41:26 -0700

    Add files via upload

---
+++ 36.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<Windows.h>
+#include <ctime>
+using namespace std;
+//function declaration
+char foodDrop(char location);
+
+
+int main() {
+	srand(time(NULL));
+	char location = 'd';
+	cout << foodDrop(location) << endl;;
+	system("pause");
+
+
+}
+char foodDrop(char location) {
+
+	int num = rand() % 100 + 1;
+
+	if (location == 'f') {
+		if (num <= 50) {
+			cout << "you got a berry!" << endl;
+			return 'b';
+		}
+		else {
+			cout << "you got a mushroom!" << endl;
+			return 'm';
+
+		}
+
+
+
+	}
+	else if (location == 'd') {
+		if (num <= 20) {
+			cout << "You got a berry!" << endl;
+			return 'b';
+		}
+		else if (num <= 40) {
+			cout << "You got a fruit!" << endl;
+			return 'f';
+		}
+		else if (num < 40) {
+			cout << "You found some cheese?" << endl;
+			return 'c';
+		}
+	}
+
+
+	else
+		cout << "invalid location" << endl;
+
+}
+
+/*if (places > 50) {
+	cout << 'f';
+}
+if (places < 50) {
+	cout << 'd';
+}
+
+return('f', 'd');*/
