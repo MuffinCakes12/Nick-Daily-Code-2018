189af6827876ec3f8c784090e8fb3a8af628576a
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-03-21 13:10:24 -0600

    Add files via upload

---
+++ MessageBox.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+#include<Windows.h>
+using namespace std;
+int main() {
+	while (1) {
+		MessageBox(NULL, ("Wowza A MessageBox"), ("Wowza"), MB_OK | MB_SYSTEMMODAL);
+}
+}
