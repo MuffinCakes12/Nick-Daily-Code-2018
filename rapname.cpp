4607de4cebd41f55b90e7b604143a0de9b9dc591
MuffinCakes12 <35504072+MuffinCakes12@users.noreply.github.com>
2018-04-18 13:17:28 -0600

    Add files via upload

--- rapname.cpp
+++ rapname.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 #include<ctime>
 #include<string>
+#include<fstream>
 using namespace std;
 char input = 'y';
 string adj[]{ "Bitter","Lemon-Flavored","Fluffy","Screeching","Lil","Slick","Shaky","Blobby","Historical","Teeny","Ancient","Gabby","Jaunty","Vivacious","Tawdry", "Testy", "Wacky", "Old" };
 string noun[]{ "Mount Kilimanjaro","Violin","flock","religion","tablecloth","Bench","Fish","Banana","Bortnite","pigtails","milk","Flipper","pen","Spoop" };
-int main() {
+int main() 
+{
+	ifstream InFile;
+	ifstream OutFile;
+	char input;
+	string SavedNames;
+	InFile.open("SavedNames.txt");
+	//OutFile << "SavedNames.txt";
+	InFile >> SavedNames;
+	InFile.close();
+	OutFile.close();
+
 	srand(time(NULL));
+	cout << "Would you like to see your previous names?" << endl;
+	cin >> input;
+	if (input == 'y')
+		cout << "Your previous names were " << SavedNames  << endl;
+	while(input!='q'){
 	cout << "Do you want to know your Rap name?" << endl;
 	while (input == 'y') {
 		cin >> input;
@@ -15,7 +32,7 @@ int main() {
 		int nouns = rand() % 10;
 		cout << adj[ad] << endl;
 		cout << noun[nouns] << endl;
-
+	}
 	}
 }
 
